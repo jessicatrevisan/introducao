39961f0ed4839eae9d1eac7b95974d81d46c8c32
jessicatrevisan <jessica.trevisan.souza@escola.pr.gov.br>
2026-08-10 11:27:09 +0000

    aulas

---
+++ lista 2/1a.c
@@ -0,0 +1,2 @@
+Caso Base:Condição de parada.Interrompe as chamadas.Evita travamentos (stack overflow).Passo 
+Recursivo:Chamada à própria função.Modifica os dados recebidos.Aproxima o problema da parada.
